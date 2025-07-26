c3030cfdb66bab293acdc44acfa65f4305a14c7d
alinaios <85871814+alinaios@users.noreply.github.com>
2025-07-26 15:47:23 +0200

    Initialise SwiftUI project and clean boilerplate

---
+++ SearchSample/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SearchSample
+//
+//  Created by AH on 2025-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Select an item")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SearchSample/SearchSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  SearchSampleApp.swift
+//  SearchSample
+//
+//  Created by AH on 2025-07-26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SearchSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
