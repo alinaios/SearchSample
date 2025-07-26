7cdc92849093954a64bcebc81b33fa1fd548bcea
alinaios <85871814+alinaios@users.noreply.github.com>
2025-07-26 19:46:38 +0200

    Add initial composition root

--- SearchSample/SearchSampleApp.swift
+++ SearchSample/SearchSampleApp.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 
+let breweriesService = URL(string: "https://api.openbrewerydb.org/v1/breweries?per_page=5")!
+let client = URLSessionHTTPClient(session: URLSession.shared)
+let service =  RemoteBreweryItemDataLoader(url: breweriesService, client: client)
+
 @main
 struct SearchSampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BrewerySearchView(viewModel: BreweryViewModel(service: service))
         }
     }
 }
